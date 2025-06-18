5aa76a041125d4e2351f6bfa2421532876d3a5a0
Isak Endresen <isen@computas.com>
2025-06-18 10:39:09 +0200

    Clean up breaking changes from upstream

--- frontend/src/pages/Experiences.tsx
+++ frontend/src/pages/Experiences.tsx
@@ -3,6 +3,7 @@ import styles from "./Experiences.module.css";
 import { CxOption, CxSelect } from "@computas/designsystem/select/react";
 import { experienceTypeMap } from "../types/experienceTypes";
 import { useExperiences } from "../hooks/useExperiences";
+import { ExperienceCard } from "../components/experiences/ExperienceCard";
 
 export default function Experiences() {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -11,13 +12,7 @@ export default function Experiences() {
   );
 
   // TODO Oppgave 1.1 of 1.2: Håndter loading og error av erfaringer
-  const { data: experiences, isLoading, error } = useExperiences();
-
-  if (isLoading)
-    return <div className={styles.loading}>Loading experiences..</div>
-
-  if (error)
-    return <div className={styles.error}>Error loading experiences :/</div>
+  const { data: experiences, isLoading: isExperiencesLoading, error: experiencesError } = useExperiences();
 
   if (!experiences || experiences.length === 0) {
     return <div className={styles.noExperiences}>No experiences found.</div>;
@@ -28,7 +23,7 @@ export default function Experiences() {
     const selectedFilter = customEvent.detail.value;
     console.log(selectedFilter);
     // TODO Oppgave 5.1: Filtrer experiences etter type
-    setSelectedExperience(selectedFilter);
+    _setSelectedExperience(selectedFilter);
   };
 
   // const filteredExperiences = () => {
@@ -87,37 +82,4 @@ export default function Experiences() {
     );
   }
 
-
-<<<<<<< HEAD
-
-  return (
-    <div className={styles.container}>
-      <div className={styles.select}>
-        <label className="cx-form-field">
-          <div className="cx-form-field__input-container">
-            <CxSelect onChange={handleSelectChange}>
-              <CxOption value="">Ingen filtrering</CxOption>
-              {Object.entries(experienceTypeMap).map(([type, data]) => (
-                <CxOption key={type} value={type}>
-                  {data.text}
-                </CxOption>
-              ))}
-            </CxSelect>
-          </div>
-        </label>
-      </div>
-      <div className={styles.experiences}>
-        {/*TODO Oppgave 3.1: Vis alle erfaringene*/}
-        {/* TODO Oppgave 4.1: Sorter erfaringene*/}
-        {filteredExperiences()
-          .sort((a, b) => new Date(b.startDate).getTime() - new Date(a.startDate).getTime())
-          .map(e => (
-            <ExperienceCard key={e.id} experience={e} />
-          )
-          )}
-      </div>
-    </div>
-  );
-=======
->>>>>>> upstream/main
 }
